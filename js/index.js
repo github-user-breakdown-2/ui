39c01add14d95ace3fdde69da455f9f123b69896
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-03-11 14:30:42 -0500

    carousel wireframe

---
+++ js/index.js
@@ -0,0 +1,36 @@
+class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.leftButton = carousel.querySelector('.left-button');
+        this.rightButton = carousel.querySelector('.right-button');
+        this.carouselItems = carousel.querySelectorAll('.card');
+        this.carouselItems[0].style.display = 'block';
+        this.currentItem = 0;
+
+        this.leftButton.addEventListener('click', _ => this.changeItem(false));
+        this.rightButton.addEventListener('click', _ => this.changeItem(true));
+    }
+
+    changeItem(goRight) {
+        this.carouselItems[this.currentItem].style.display = 'none';
+        if(goRight) {
+            if(this.currentItem + 1 > this.carouselItems.length - 1) {
+                this.currentItem = 0;
+            } else {
+                this.currentItem++;
+            }
+        } else {
+            if(this.currentItem - 1 < 0) {
+                this.currentItem = this.carouselItems.length - 1;
+            } else {
+                this.currentItem--;
+            }
+        }
+        this.carouselItems[this.currentItem].style.display = 'block';
+    }
+}
+
+document.querySelectorAll('.features').forEach(function(carousel){
+    console.log("huh")
+    return new Carousel(carousel);
+});
